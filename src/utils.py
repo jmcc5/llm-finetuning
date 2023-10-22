42c834978f8507f01e2e0cf2ff4b02cc7df61844
Harrison Floam <harrisonfloam@gmail.com>
2023-10-22 19:30:10 -0400

    restructure to use /src

--- src/utils.py
+++ src/utils.py
@@ -7,4 +7,4 @@ from pathlib import Path
 
 def get_project_root() -> Path:
     """Return absolute path to project root. Modify if file is moved from root."""
-    return Path(__file__).parent
+    return Path(__file__).parent.parent
