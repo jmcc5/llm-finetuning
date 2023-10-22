ddf31b5edfd4721a3329362f5cbf88a8bb666545
Harrison Floam <harrisonfloam@gmail.com>
2023-10-22 18:01:25 -0400

    refactor data.py and add methods to save and return models in opt.py

--- data/data.py
+++ data/data.py
@@ -11,42 +11,31 @@ from datasets import load_dataset, load_from_disk
 # Import modules
 from utils import get_project_root
 
-
-def get_mnli():
-    """Download MNLI dataset to disk"""
-    dataset = load_dataset("glue", "mnli")
-    filepath = os.path.join(get_project_root(), 'data/mnli')
-    dataset.save_to_disk(filepath)
+        
+def download_dataset(dataset_name):
+    """Load specified huggingface dataset and save to disk. MNLI, COLA and HANS supported."""
+    if dataset_name == "mnli" or dataset_name == "cola":
+        dataset = load_dataset("glue", dataset_name)
+    elif dataset_name == "hans":
+        dataset = load_dataset(dataset_name)
+    else:
+        raise ValueError(f"Dataset {dataset_name} not supported.")
+    filepath = os.path.join(get_project_root(), 'data', dataset_name)
+    if not os.path.exists(filepath):
+        dataset.save_to_disk(filepath)
     
-def get_hans():
-    """Download HANS dataset to disk"""
-    dataset = load_dataset("hans")
-    filepath = os.path.join(get_project_root(), 'data/hans')
-    dataset.save_to_disk(filepath)
-    
-def get_cola():
-    """Download COLA dataset to disk"""
-    dataset = load_dataset("glue", "cola")
-    filepath = os.path.join(get_project_root(), 'data/cola')
-    dataset.save_to_disk(filepath)
-    
-def get_in_domain():
+def get_in_domain(dataset_name='mnli', set_name='train'):
     """Returns huggingface Dataset for In Domain"""
-    dataset_name = 'mnli'
-    set_name = 'train'
     filepath = os.path.join(get_project_root(), 'data', dataset_name, set_name)
+    if not os.path.exists(filepath):
+        download_dataset(dataset_name=dataset_name)
     in_domain = load_from_disk(filepath)
     return in_domain
-
-def get_out_domain():
-    """Returns hugginface Dataset for Out of Domain"""
-    dataset_name = 'hans'
-    set_name = 'validation'
+        
+def get_out_domain(dataset_name='hans', set_name='validation'):
+    """Returns huggingface Dataset for Out of Domain"""
     filepath = os.path.join(get_project_root(), 'data', dataset_name, set_name)
+    if not os.path.exists(filepath):
+        download_dataset(dataset_name=dataset_name)
     out_domain = load_from_disk(filepath)
     return out_domain
-
-if __name__ == "__main__":
-    get_mnli()
-    get_hans()
-    get_cola()
--- models/opt.py
+++ models/opt.py
@@ -4,6 +4,13 @@ https://huggingface.co/facebook/opt-125m
 https://huggingface.co/facebook/opt-350m
 """
 
+# Import Libraries
+import os
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+# Import modules
+from utils import get_project_root
+
 # # Use a pipeline as a high-level helper
 # from transformers import pipeline
 
@@ -14,3 +21,39 @@ https://huggingface.co/facebook/opt-350m
 
 # tokenizer = AutoTokenizer.from_pretrained("facebook/opt-125m")
 # model = AutoModelForCausalLM.from_pretrained("facebook/opt-125m")
+
+def download_model(model_name):
+    """Load specified huggingface model and save to disk. opt-125m and opt-250m supported."""
+    if model_name == "opt-125m" or model_name == "opt-350m":
+        tokenizer = AutoTokenizer.from_pretrained(f"facebook/{model_name}")
+        model = AutoModelForCausalLM.from_pretrained(f"facebook/{model_name}")
+    else:
+        raise ValueError(f"Model {model_name} not supported.")
+    filepath = os.path.join(get_project_root(), 'models', model_name)
+    filepath_tokenizer = os.path.join(filepath, 'tokenizer')
+    filepath_model = os.path.join(filepath, 'model')
+    if not os.path.exists(filepath):
+        tokenizer.save_pretrained(filepath_tokenizer)
+        model.save_pretrained(filepath_model)
+        
+def get_opt125():
+    """Returns baseline opt-125m tokenizer and model"""
+    filepath = os.path.join(get_project_root(), 'models', 'opt125')
+    if not os.path.exists(filepath):
+        download_model('opt-125m')
+    filepath_tokenizer = os.path.join(filepath, 'tokenizer')
+    filepath_model = os.path.join(filepath, 'model')
+    tokenizer = AutoTokenizer.from_pretrained(filepath_tokenizer)
+    model = AutoModelForCausalLM.from_pretrained(filepath_model)
+    return tokenizer, model
+
+def get_opt350():
+    """Returns baseline opt-350m tokenizer and model"""
+    filepath = os.path.join(get_project_root(), 'models', 'opt350')
+    if not os.path.exists(filepath):
+        download_model('opt-350m')
+    filepath_tokenizer = os.path.join(filepath, 'tokenizer')
+    filepath_model = os.path.join(filepath, 'model')
+    tokenizer = AutoTokenizer.from_pretrained(filepath_tokenizer)
+    model = AutoModelForCausalLM.from_pretrained(filepath_model)
+    return tokenizer, model
