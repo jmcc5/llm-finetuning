99598dc557126b5d0de51e1d0fb0a2b1f91370c0
Harrison Floam <harrisonfloam@gmail.com>
2023-10-22 20:58:26 -0400

    modify baseline filenames

--- src/fine_tuners/baseline.py
+++
@@ -1,4 +0,0 @@
-"""
-Baseline fine-tuning methods from “Few-shot Fine-tuning vs. In-context Learning: A Fair Comparison and Evaluation”, Mosbach et al.
-https://huggingface.co/docs/transformers/training
-"""
---
+++ src/fine_tuners/fewshot.py
@@ -0,0 +1,13 @@
+"""
+Fewshot fine-tuning method from “Few-shot Fine-tuning vs. In-context Learning: A Fair Comparison and Evaluation”, Mosbach et al.
+https://aclanthology.org/2023.findings-acl.779.pdf
+https://huggingface.co/docs/transformers/training
+
+Few-Shot Fine-tuning (FT):
+- Few-shot: randomly sampled n in {2, 16, 32, 64, 128} examples.
+- Minimal pattern: append question mark to each example.
+- Verbalizer: "Yes" and "No" labels for NLI and QQP tasks.
+- Fine-tuning: 40 epochs, learning rate of 1e-5, linear increase for initial 10% of steps, then constant.
+"""
+
+
