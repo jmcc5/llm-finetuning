84cda81a25c030b7b53fc840d9c98c41422a06dd
Harrison Floam <harrisonfloam@gmail.com>
2023-10-22 23:15:08 -0400

    add random subsets method for FSFT

--- src/data/utils.py
+++ src/data/utils.py
@@ -3,14 +3,16 @@ Utility functions for data loading and processing
 """
 
 # Import Libraries
+import random
 
 
 def remove_neutral_labels(dataset):
     """Remove all rows from the MNLI dataset where the label is 1."""
     
-    filtered_dataset = dataset.filter(lambda example: example['label'] != 1)
+    filtered_dataset = dataset.filter(lambda example: example['label'] != 1)    # Remove neutral labels 
+    relabeled_dataset = filtered_dataset.map(lambda example: {'label': 1 if example['label'] == 2 else example['label']})   # Map 2 to 1 to create binary distribution
     
-    return filtered_dataset
+    return relabeled_dataset
 
 def isolate_lexical_overlap(dataset):
     """Isolate rows with heuristic 'lexical_overlap' in HANS dataset."""
@@ -18,3 +20,20 @@ def isolate_lexical_overlap(dataset):
     filtered_dataset = dataset.filter(lambda example: example['heuristic'] == 'lexical_overlap')
     
     return filtered_dataset
+
+def get_random_subsets(dataset, sample_sizes=[2, 16, 32, 64, 128], num_samples=10):
+    """Returns a dictionary of randomly sampled datasets, indexed by sample_size"""
+    
+    subsets = {}
+
+    # Loop through each sample size
+    for size in sample_sizes:
+        subsets[size] = []
+
+        # Loop to create 'num_samples' random subsets
+        for _ in range(num_samples):
+            random_indices = random.sample(range(len(dataset)), size)
+            subset = dataset.select(random_indices)
+            subsets[size].append(subset)
+
+    return subsets
