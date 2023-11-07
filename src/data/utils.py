c8dbdb352690a202a4c92236a14c87028ad5483f
Harrison Floam <harrisonfloam@gmail.com>
2023-11-06 21:20:30 -0500

    add batch handling to data/utils functions

--- src/data/utils.py
+++ src/data/utils.py
@@ -9,15 +9,24 @@ import random
 def remove_neutral_labels(dataset):
     """Remove all rows from the MNLI dataset where the label is 1."""
     
-    filtered_dataset = dataset.filter(lambda example: example['label'] != 1)    # Remove neutral labels 
-    relabeled_dataset = filtered_dataset.map(lambda example: {'label': 1 if example['label'] == 2 else example['label']})   # Map 2 to 1 to create binary distribution
+    def filter_labels(batch):
+        return [label != 1 for label in batch['label']]
+    
+    def remap_labels(batch):
+        return {'label': [1 if label == 2 else label for label in batch['label']]}
+    
+    filtered_dataset = dataset.filter(filter_labels, batched=True)  # Remove neutral labels 
+    relabeled_dataset = filtered_dataset.map(remap_labels, batched=True)    # Map 2 to 1 to create binary distribution
     
     return relabeled_dataset
 
 def isolate_lexical_overlap(dataset):
     """Isolate rows with heuristic 'lexical_overlap' in HANS dataset."""
     
-    filtered_dataset = dataset.filter(lambda example: example['heuristic'] == 'lexical_overlap')
+    def filter_heuristic(batch):
+        return [heuristic == 'lexical_overlap' for heuristic in batch['heuristic']]
+    
+    filtered_dataset = dataset.filter(filter_heuristic, batched=True)
     
     return filtered_dataset
 
