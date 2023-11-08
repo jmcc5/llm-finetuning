9ed74637e42782506d5555911061449d251b0646
Harrison Floam <harrisonfloam@gmail.com>
2023-11-08 13:42:47 -0500

    rename subfunctions

--- src/data/utils.py
+++ src/data/utils.py
@@ -10,24 +10,24 @@ def remove_neutral_labels(dataset):
     """Remove all rows from the MNLI dataset where the label is 1."""
     #TODO: make subfunction names uniform (format_batch)
     
-    def filter_labels(batch):
+    def filter_batch(batch):
         return [label != 1 for label in batch['label']]
     
-    def remap_labels(batch):
+    def remap_batch(batch):
         return {'label': [1 if label == 2 else label for label in batch['label']]}
     
-    filtered_dataset = dataset.filter(filter_labels, batched=True)  # Remove neutral labels 
-    relabeled_dataset = filtered_dataset.map(remap_labels, batched=True)    # Map 2 to 1 to create binary distribution
+    filtered_dataset = dataset.filter(filter_batch, batched=True)  # Remove neutral labels 
+    relabeled_dataset = filtered_dataset.map(remap_batch, batched=True)    # Map 2 to 1 to create binary distribution
     
     return relabeled_dataset
 
 def isolate_lexical_overlap(dataset):
     """Isolate rows with heuristic 'lexical_overlap' in HANS dataset."""
     
-    def filter_heuristic(batch):
+    def filter_batch(batch):
         return [heuristic == 'lexical_overlap' for heuristic in batch['heuristic']]
     
-    filtered_dataset = dataset.filter(filter_heuristic, batched=True)
+    filtered_dataset = dataset.filter(filter_batch, batched=True)
     
     return filtered_dataset
 
