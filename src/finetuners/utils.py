1bb832653a666360898948cb47aaf7012f38f3ce
Harrison Floam <harrisonfloam@gmail.com>
2023-11-08 14:35:02 -0500

    suppress dataset progress bar during finetuning

--- src/finetuners/utils.py
+++ src/finetuners/utils.py
@@ -5,26 +5,29 @@ Utility functions for fine-tuning
 # Import Libraries
 import numpy as np
 import evaluate
+from datasets.utils import disable_progress_bar
 
 
 def apply_minimal_pattern(dataset):
-    """Apply the minimal pattern '{premise} {hypothesis}?'. Currently supports MNLI."""
-    
+    """Apply the minimal pattern '{premise} {hypothesis}?'. Currently supports MNLI."""   
     def format_batch(batch):
         # Apply the minimal pattern to the entire batch and return the modified batch
         batch['text'] = [premise + " " + hypothesis + "?" for premise, hypothesis in zip(batch['premise'], batch['hypothesis'])]
         return batch
-
+    
+    disable_progress_bar() 
     dataset = dataset.map(format_batch, batched=True)
     
     return dataset
 
 def tokenize_dataset(dataset, tokenizer, max_length=512):
     """Tokenize input dataset. Designed for use after minimal pattern is applied."""
+    disable_progress_bar()
     def tokenize_function(examples):
         tokenized_examples = tokenizer(examples['text'], truncation=True, padding='max_length', max_length=max_length)
         return tokenized_examples
     
+    disable_progress_bar() 
     dataset = dataset.map(tokenize_function, batched=True)
     
     return dataset
