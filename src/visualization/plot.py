18be882c5d3a2b4f1139320a1a4aa5b0e5cbf963
Harrison Floam <harrisonfloam@gmail.com>
2023-11-13 14:15:26 -0500

    add plot_in_out_domain function

--- src/visualization/plot.py
+++ src/visualization/plot.py
@@ -1,3 +1,49 @@
 """
 Functions for plotting fine-tuning results
 """
+
+# Import libraries
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Import Modules
+from src.utils import get_project_root
+
+def plot_in_out_domain(logfile, metric):
+    """Plot in domain vs. out of domain metrics from a log file"""
+    # Read log file
+    logfilepath = os.path.join(get_project_root(), 'logs', logfile)
+    log_df = pd.read_csv(logfilepath)
+    
+    plt.figure(figsize=(6, 6))
+    # plt.xlim(axes_min, axes_max)
+    # plt.ylim(axes_min, axes_max)
+
+    sample_sizes = log_df['sample_size'].unique()
+
+    # Plot in-domain and out-of-domain metrics for each sample size
+    for size in sample_sizes:
+        subset = log_df[log_df['sample_size'] == size]
+        
+        avg_in_metric = subset[f'eval_in_{metric}'].mean()
+        avg_out_metric = subset[f'eval_out_{metric}'].mean()
+
+        plt.scatter(avg_in_metric, avg_out_metric, label=f'{size}-shot', marker='+', s=200)
+    
+    # Equalize axes and plot diagonal line
+    x_min, x_max = plt.xlim()
+    y_min, y_max = plt.ylim()
+    axes_range = (min(x_min, y_min), max(x_max, y_max))
+    plt.xlim(axes_range)
+    plt.ylim(axes_range)
+    
+    plt.plot(axes_range, axes_range, 'k--', alpha=0.2)
+
+    plt.title(f'In-Domain vs. Out-of-Domain {metric.capitalize()}')
+    plt.xlabel('In-Domain')
+    plt.ylabel('Out-of-Domain')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
