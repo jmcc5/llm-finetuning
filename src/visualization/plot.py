d793b6a3ad1d93ad819191b48c0f580de569ed90
Harrison Floam <harrisonfloam@gmail.com>
2023-12-05 16:40:23 -0500

    create plot_in_out_domain_subplots and figures_test.ipynb

--- src/visualization/plot.py
+++ src/visualization/plot.py
@@ -25,6 +25,7 @@ def plot_in_out_domain(logfile, metric):
 
     # Plot in-domain and out-of-domain metrics for each sample size
     for size in sample_sizes:
+        # for model_name in 
         subset = log_df[log_df['sample_size'] == size]
         
         avg_in_metric = subset[f'eval_in_{metric}'].mean()
@@ -43,14 +44,70 @@ def plot_in_out_domain(logfile, metric):
     
     num_trials = len(subset)
     model_name = log_df['model_name'][0]
+    finetuning_method = logfile.split('_')[0]
 
-    plt.title(f'In-Domain vs. Out-of-Domain {metric.capitalize()} ({num_trials} trials, {model_name})')
+    plt.title(f'{metric.capitalize()} ({finetuning_method}, {num_trials} trials, {model_name})')
     plt.xlabel('In-Domain')
     plt.ylabel('Out-of-Domain')
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
     plt.show()
+    
+def plot_in_out_domain_subplots(logfiles, metrics=['accuracy', 'runtime', 'peak_memory_gb', 'loss'], group_by=None):
+    """Plot in vs. out-of-domain data in separate subplots for a desired grouping"""
+    
+    #TODO: draw line for zeroshot baseline
+    #TODO: draw diagonal line and square plots
+    
+    # Combine logfiles into single dataframe
+    combined_df = pd.DataFrame()
+    for logfile in logfiles:
+        logfilepath = os.path.join(get_project_root(), 'logs', logfile)
+        finetuning_method = logfile.split('_')[0]
+        temp_df = pd.read_csv(logfilepath)
+        temp_df['finetuning_method'] = finetuning_method
+        combined_df = pd.concat([combined_df, temp_df])
+    
+    # Get members of grouping
+    if group_by:
+        unique_groups = combined_df[group_by].unique()
+    else:
+        unique_groups = ['All Data']
+    
+    num_cols = len(metrics)
+    
+    for group in unique_groups:
+        fig, axes = plt.subplots(1, num_cols, figsize=(5 * num_cols, 5))
+        axes = axes.flatten()
+
+        for subplot, metric in enumerate(metrics):
+            ax = axes[subplot]
+            if group_by:
+                group_df = combined_df[combined_df[group_by] == group]
+            else:
+                group_df = combined_df
+
+            # Use pandas groupby to handle the complex grouping logic
+            for (model_name, sample_size, finetuning_method), subset in group_df.groupby(['model_name', 'sample_size', 'finetuning_method']):
+                avg_in_metric = subset[f'eval_in_{metric}'].mean()
+                avg_out_metric = subset[f'eval_out_{metric}'].mean()
+
+                label = f"{finetuning_method} ({sample_size}, {model_name})"
+                ax.scatter(avg_in_metric, avg_out_metric, label=label, marker='+', s=200)
+
+            ax.set_title(f'{metric.capitalize()} - {group}')
+            ax.set_xlabel('In-Domain')
+            ax.set_ylabel('Out-of-Domain')
+            ax.grid(True)
+
+        handles, labels = ax.get_legend_handles_labels()
+        if handles and labels:
+            fig.legend(handles, labels, bbox_to_anchor=(1, 0.53), loc='lower left', ncol=1)
+
+        plt.tight_layout()
+        plt.show()
+
     
 def plot_learning_curves(logfile, subplot_cols=None):
     """Plot learning curves from a log file"""
